1e81a095d10b85bdd5683add79bcd81f33c1c203
TimB44 <tblamires22@gmail.com>
2025-12-09 23:23:08 -0700

    day 1 part 2

--- src/day01.rs
+++ src/day01.rs
@@ -2,6 +2,39 @@ use std::io::{self, BufRead};
 
 const MODULO: i32 = 100;
 
+#[derive(Clone, Copy)]
+enum Direction {
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn to_sign(self) -> i32 {
+        match self {
+            Direction::Left => -1,
+            Direction::Right => 1,
+        }
+    }
+}
+
+fn distance_to_zero(pos: i32, dir: Direction) -> i32 {
+    assert!(pos < MODULO);
+    match dir {
+        Direction::Left if pos == 0 => MODULO,
+        Direction::Left => pos,
+        Direction::Right => MODULO - pos,
+    }
+}
+
+fn count_zero_hits(pos: i32, dir: Direction, amount: i32) -> i32 {
+    let dist = distance_to_zero(pos, dir);
+    if dist > amount {
+        0
+    } else {
+        1 + (amount - dist) / MODULO
+    }
+}
+
 fn main() -> io::Result<()> {
     let stdin = io::stdin().lock();
     let mut cur_pos = 50;
@@ -9,14 +42,15 @@ fn main() -> io::Result<()> {
 
     for line_res in stdin.lines() {
         let line = line_res?;
-        let raw_line = line.as_bytes();
-        let sign = if raw_line[0] == b'R' { 1 } else { -1 };
+        let dir = if &line[..1] == "R" {
+            Direction::Right
+        } else {
+            Direction::Left
+        };
         let n: i32 = line[1..].parse().unwrap();
-        cur_pos += n * sign;
-        cur_pos %= MODULO;
-        if cur_pos == 0 {
-            zero_count += 1;
-        }
+        zero_count += count_zero_hits(cur_pos, dir, n);
+        cur_pos += n * dir.to_sign();
+        cur_pos = cur_pos.rem_euclid(MODULO);
     }
     println!("Zero count = {zero_count}");
 
