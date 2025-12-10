83429846de2d1fe08620c7bfba9579ac17e3d702
TimB44 <tblamires22@gmail.com>
2025-12-10 03:12:16 -0700

    day 4 part 1

---
+++ src/day04.rs
@@ -0,0 +1,40 @@
+use std::{
+    io::{stdin, BufRead},
+    iter::repeat,
+};
+
+const SURROUNDED_THRESHOLD: usize = 4;
+
+fn main() {
+    let grid: Vec<Vec<bool>> = stdin()
+        .lock()
+        .lines()
+        .map(Result::unwrap)
+        .map(|line| line.bytes().map(|b| b == b'@').collect())
+        .collect();
+    let rows = grid.len();
+    let cols = grid[0].len();
+
+    let is_open = |row_i: usize, col_i: usize| -> bool {
+        let start_row_i = row_i.saturating_sub(1);
+        let start_col_i = col_i.saturating_sub(1);
+
+        let end_row_i = (row_i + 2).min(rows);
+        let end_col_i = (col_i + 2).min(cols);
+
+        (start_row_i..end_row_i)
+            .flat_map(|cur_row_i| repeat(cur_row_i).zip(start_col_i..end_col_i))
+            .filter(|(r, c)| !(*r == row_i && *c == col_i) && grid[*r][*c])
+            .count()
+            < SURROUNDED_THRESHOLD
+    };
+    let surrounded_count = grid
+        .iter()
+        .enumerate()
+        .flat_map(|(row_i, row)| row.iter().enumerate().map(move |(col_i, _)| (row_i, col_i)))
+        .filter(|&(row_i, col_i)| grid[row_i][col_i])
+        .filter(|&(row_i, col_i)| is_open(row_i, col_i))
+        .count();
+
+    println!("Surrounded count = {surrounded_count}");
+}
