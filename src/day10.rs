0942371fcf4f485e33b5f3d956ea82c6aab05b0b
TimB44 <tblamires22@gmail.com>
2025-12-15 02:48:45 -0700

    day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,45 +1,78 @@
-use std::{
-    collections::{HashSet, VecDeque},
-    io::stdin,
-};
+use std::{collections::HashMap, io::stdin};
 
 fn min_path(line: String) -> usize {
     let parts: Vec<_> = line.split_whitespace().collect();
-    let edges: Vec<u64> = parts[1..parts.len() - 1]
+    let end_str = parts[parts.len() - 1];
+    let end: Vec<usize> = end_str[1..end_str.len() - 1]
+        .split(',')
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<_>>();
+    let edges: Vec<usize> = parts[1..parts.len() - 1]
         .iter()
         .map(|s| {
             s[1..s.len() - 1]
                 .split(',')
-                .fold(0, |m, n| m | 1 << n.parse::<u64>().unwrap())
+                .fold(0, |m, n| m | 1 << n.parse::<usize>().unwrap())
         })
         .collect();
 
-    let end_str = parts[0];
-    let end: u64 = end_str[1..end_str.len() - 1]
-        .bytes()
-        .map(|b| if b == b'#' { 1 } else { 0 })
-        .rev()
-        .fold(0, |m, b| (m << 1) | b);
-
-    let mut seen = HashSet::from([0]);
-    let mut q = VecDeque::from([(0, 0)]);
-
-    loop {
-        let (state, cost) = q.pop_front().unwrap();
-        if state == end {
-            return cost;
-        }
-        for new_state in edges
-            .iter()
-            .map(|&e| e ^ state)
-            .filter(|&new_state| seen.insert(new_state))
-        {
-            q.push_back((new_state, cost + 1));
-        }
-    }
+    min_sum(&edges, end, &mut HashMap::new())
 }
+
+fn min_sum(edges: &[usize], goal: Vec<usize>, cache: &mut HashMap<Vec<usize>, usize>) -> usize {
+    if goal.iter().all(|g| *g == 0) {
+        return 0;
+    }
+    if let Some(res) = cache.get(&goal) {
+        return *res;
+    }
+
+    let cost = (0..(1 << edges.len()))
+        .map(|bit_set: usize| {
+            (
+                bit_set.count_ones(),
+                edges
+                    .iter()
+                    .enumerate()
+                    .filter(|(i, _)| ((bit_set >> i) & 1 == 1))
+                    .fold(vec![0; goal.len()], |mut v, (_, n)| {
+                        let mut n = *n;
+                        let mut i = 0;
+                        while n != 0 {
+                            v[i] += n & 1;
+                            n /= 2;
+                            i += 1;
+                        }
+                        v
+                    }),
+            )
+        })
+        .filter(|(_, v)| {
+            v.iter()
+                .zip(goal.iter())
+                .all(|(cur, goal)| cur <= goal && (goal - *cur) % 2 == 0)
+        })
+        .map(|(cost, next_move)| {
+            let next_goal: Vec<usize> = next_move
+                .into_iter()
+                .zip(goal.iter())
+                .map(|(i, &m)| m.saturating_sub(i) / 2)
+                .collect::<Vec<_>>();
+            (cost as usize).saturating_add(min_sum(edges, next_goal, cache).saturating_mul(2))
+        })
+        .min()
+        .unwrap_or(usize::MAX);
+    cache.insert(goal, cost);
+
+    cost
+}
+
 fn main() {
-    let sum: usize = stdin().lines().map(Result::unwrap).map(min_path).sum();
+    let sum: usize = stdin()
+        .lines()
+        .map(Result::unwrap)
+        .map(min_path)
+        .sum::<usize>();
 
     println!("Path sum = {sum}");
 }
